7ba429270ec4542991f4a0ee22f3502a33a865ba
Aaron Wu <82164146+awu0@users.noreply.github.com>
2024-12-09 16:57:43 -0500

    added back FAB for details

--- app/src/main/java/com/awu0/notetakeoff/ui/note_details/NoteDetailsScreen.kt
+++ app/src/main/java/com/awu0/notetakeoff/ui/note_details/NoteDetailsScreen.kt
@@ -3,16 +3,20 @@ package com.awu0.notetakeoff.ui.note_details
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -25,6 +29,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -108,6 +113,21 @@ fun NoteDetailsScreen(
                 NoteDetailsBody(
                     note = uiState.value.noteDetails.toNote(),
                     modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+                )
+
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_note_details)))
+            }
+
+            FloatingActionButton(
+                onClick = { navigateToEditNote(uiState.value.noteDetails.id) },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(dimensionResource(R.dimen.padding_medium))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_note)
                 )
             }
 
